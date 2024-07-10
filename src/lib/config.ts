f76fe1f1b3959bfd1dbcaa423ec968876257f608
Antonios Papadakis <bdpaboi123@gmail.com>
2024-07-10 11:31:39 +0300

    Bump Version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -57,7 +57,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
     language: "typescript",
     openapiDocVersion: "3.1.1",
-    sdkVersion: "3.1.1",
+    sdkVersion: "3.1.2",
     genVersion: "2.361.10",
-    userAgent: "speakeasy-sdk/typescript 3.1.1 2.361.10 3.1.1 @aurora-interactive/chatbot-api-sdk",
+    userAgent: "speakeasy-sdk/typescript 3.1.2 2.361.10 3.1.1 @aurora-interactive/chatbot-api-sdk",
 } as const;
